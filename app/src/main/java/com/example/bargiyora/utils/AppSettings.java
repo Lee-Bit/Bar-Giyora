6cc6bea69a1073e01d66534e006b69f21447a79d
Lee Biton <leekalfon@gmail.com>
2020-10-22 12:20:27 +0300

    Add global parameters

---
+++ app/src/main/java/com/example/bargiyora/utils/AppSettings.java
@@ -0,0 +1,8 @@
+package com.example.bargiyora.utils;
+
+public class AppSettings {
+    public static final String AROUND_US_CATEGORY_RESTAURANTS = "1111";
+    public static final String AROUND_US_CATEGORY_POOLS = "2222";
+    public static final String AROUND_US_CATEGORY_TRIPS = "3333";
+    public static final String AROUND_US_CATEGORY_OTHERS = "4444";
+}
